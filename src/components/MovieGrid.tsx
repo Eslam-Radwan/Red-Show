d0afd6aa6afa1fdb4eecac6a8143615b798fd93c
radwan <eslamradwan145@gmail.com>
2025-06-08 02:30:43 +0300

    Refactor MovieGrid component to handle loading and error states

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -4,18 +4,40 @@ import type { movie } from "../types/movie";
 
 const IMAGE_URL = import.meta.env.VITE_IMAGE_URL;
 const MovieGrid = () => {
-    const { data, isLoading, error } = useQuery({ queryKey: ['popularMovies'], queryFn: fetchPopularMovies })
+    const { data, isLoading, error , isError} = useQuery({ queryKey: ['popularMovies'], queryFn: fetchPopularMovies })
 
 
+    let content;
+    if (isLoading)
+    {
+        content =  <> Loading...</>
+            
+        
+    }
+    else if (isError)
+    {
+        content = <>
+        <p>Error Occured</p>
+        <p>{error.message}</p>
+        </>
+    }
+    else if (data)
+    {
+        console.log(data);
+        
+        content = data?.map((movie: movie) => {
+                        return (
+                        <img key={movie.id} className="w-[200px] rounded-2xl" src={`${IMAGE_URL}/w200/${movie.poster_path}`} alt="" />
+                        )
+                    })
+        
+    }
+    
     return (
         <div className="max-w-[1440px] mx-auto">
             <div className="flex flex-wrap gap-10">
                 {
-                    data?.map((movie: movie) => {
-                        return (
-                        <img className="w-[200px] rounded-2xl" src={`${IMAGE_URL}/${movie.poster_path}`} alt="" />
-                        )
-                    })
+                    content
                 }
             </div>
         </div>
